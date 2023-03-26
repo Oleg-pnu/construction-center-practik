367ce19888b09ece22e98914716d74206ced4e63
OlegPavliv <78442766+Oleg-pnu@users.noreply.github.com>
2023-03-27 02:39:35 +0300

    Footer beta

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NaviBar from './Components/Navibar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { Home } from './Home';
+import { Users } from './Users';
+import { About } from './About';
 
 function App() {
 	return (
-		<div className="App">
-
-		</div>
+		<>
+			<Router>
+				<NaviBar />
+				<Routes>
+					<Route exect path="/" element={<Home />} />
+					<Route exect path="/users" element={<Users />} />
+					<Route exect path="/about" element={<About />} />
+				</Routes>
+			</Router>
+		</>
 	);
 }
 
