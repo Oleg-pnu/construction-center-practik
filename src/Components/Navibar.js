5acf18f0617b5dc2d93a18b24aaafb92bc8c5637
OlegPavliv <78442766+Oleg-pnu@users.noreply.github.com>
2023-04-05 04:32:30 +0300

    #3 Registration form

--- src/Components/Navibar.js
+++ src/Components/Navibar.js
@@ -1,8 +1,9 @@
 import { hover } from "@testing-library/user-event/dist/hover";
-import React from "react";
-import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Navbar, Nav, Button, Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import styled from "styled-components";
+import styled from 'styled-components';
+import Modal from 'react-bootstrap/Modal';
 
 const Styles = styled.div`
 a, .navbar - brand, .navbar - nav, .nav - link {
@@ -13,7 +14,13 @@ a, .navbar - brand, .navbar - nav, .nav - link {
 }
 `
 
-function NaviBar() {
+export default function NaviBar() {
+
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
 	return (
 		<>
 			<Styles>
@@ -28,15 +35,34 @@ function NaviBar() {
 								<Nav.Link><Link to="/about">About</Link></Nav.Link>
 							</Nav>
 							<Nav>
-								<Button variant="primary" className="mr-2" >Log In</Button>
-								<Button variant="primary">Sign out</Button>
+								<Button variant="primary" className="mr-2" onClick={handleShow}>Log In</Button>
+								<Button variant="primary" onClick={handleShow}>Sign out</Button>
 							</Nav>
 						</Navbar.Collapse>
 					</Container>
 				</Navbar>
 			</Styles>
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Log in</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<Form.Group controlId="formBasicEmail">
+							<Form.Label>Email Adress</Form.Label>
+							<Form.Control type="email" placeholder="Enter email" />
+							<Form.Text className="text-muted">We'll never share your email whith anyone else.</Form.Text>
+						</Form.Group>
+						<Form.Group controlId="formBasicPassword">
+							<Form.Label>Password</Form.Label>
+							<Form.Control type="password" placeholder="Enter password" />
+						</Form.Group>
+						<Form.Group controlId="formBasicCheckbox">
+							<Form.Check type="checkbox" label="Remeber me" />
+						</Form.Group>
+					</Form>
+				</Modal.Body>
+			</Modal >
 		</>
 	);
 }
-
-export default NaviBar;
