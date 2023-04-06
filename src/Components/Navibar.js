64ff6b6d9b37a6fb4d2602ac84e6314af96182bc
OlegPavliv <78442766+Oleg-pnu@users.noreply.github.com>
2023-04-06 18:57:39 +0300

    #3-Error-correction.

--- src/Components/Navibar.js
+++ src/Components/Navibar.js
@@ -1,5 +1,5 @@
 import { hover } from "@testing-library/user-event/dist/hover";
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { Navbar, Nav, Button, Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
@@ -49,16 +49,16 @@ export default function NaviBar() {
 				<Modal.Body>
 					<Form>
 						<Form.Group controlId="formBasicEmail">
-							<Form.Label>Email Adress</Form.Label>
+							<Form.Label>Email Address</Form.Label>
 							<Form.Control type="email" placeholder="Enter email" />
-							<Form.Text className="text-muted">We'll never share your email whith anyone else.</Form.Text>
+							<Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
 						</Form.Group>
 						<Form.Group controlId="formBasicPassword">
 							<Form.Label>Password</Form.Label>
 							<Form.Control type="password" placeholder="Enter password" />
 						</Form.Group>
 						<Form.Group controlId="formBasicCheckbox">
-							<Form.Check type="checkbox" label="Remeber me" />
+							<Form.Check type="checkbox" label="Remember me" />
 						</Form.Group>
 					</Form>
 				</Modal.Body>
