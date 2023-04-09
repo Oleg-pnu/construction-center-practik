1138ebbfb9304b552e5b54cbd73ccaa8cc13b531
OlegPavliv <78442766+Oleg-pnu@users.noreply.github.com>
2023-04-09 22:57:57 +0300

    #5 Static signature

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,8 @@ import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import Static1 from './Assets/Static1.jpg';
 import Static2 from './Assets/Static2.jpg';
 import Static3 from './Assets/Static3.jpg';
+import House from './Assets/House.jpg';
+
 
 export const Home = () => (
 	<>
@@ -46,6 +48,28 @@ export const Home = () => (
 				</Col>
 			</Row>
 		</Container>
-
+		<Container>
+			<Row>
+				<Col mg={7}>
+					<img src={House} height={400} />
+				</Col>
+				<Col mg={5}>
+					<p>
+						"BudFox" has been engaged in construction in the south of Ukraine for more than 30 years.
+						We built 60 buildings with a total area of 800,000 m², including not only residential complexes,
+						but also Orthodox churches, a synagogue, an Arab cultural center, shopping and office centers.
+						We do not just build residential complexes, but take responsibility for their further maintenance.
+						The management company "Nash Dom" created by us serves more than 750,000 m² of housing,
+						in which about 25,000 Odessa residents live. "BudFox" unites more than 2,200 industry professionals,
+						the first in Odessa to use innovative software and engineering construction solutions: BIM modeling,
+						touch-tables for the demonstration of apartments in sales departments,
+						a ventilation system with heat recovery, three-layer fire-resistant facade panels,
+						solar systems, energy-efficient boilers, "Smart House" etc. In general,
+						about 10,000 people are involved in the activities of our company,
+						taking into account contractors and one-off projects.
+					</p>
+				</Col>
+			</Row>
+		</Container>
 	</>
 )
